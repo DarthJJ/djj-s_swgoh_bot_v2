1fcd01e17613cfe256e0bf7d0f0a2827816e8092
Nick Janssen <nickrjanssen91@gmail.com>
2021-04-29 11:09:56 +0200

    Always exit 0

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/bash
 
-set -e
-
-# keep track of the last executed command
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
-
 search_terms='bot.jar'
 
 kill $(ps aux | grep "$search_terms" | grep -v 'grep' | awk '{print $2}')
+
+exit 0;
